99d84487b7ac058417c2577044a7f12ba163f205
Alchemist <rhithick02@gmail.com>
2020-08-13 19:59:13 +0530

    Update

--- Game_dev/shmup.py
+++ Game_dev/shmup.py
@@ -14,6 +14,14 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 YELLOW = (255, 255, 0)
 
+font_name = pygame.font.match_font('arial')
+def draw_text(text, surf, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surf = font.render(text, True, WHITE)
+    text_rect = text_surf.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surf, text_rect)
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -44,16 +52,33 @@ class Player(pygame.sprite.Sprite):
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.transform.scale(meteor, (50,42))
-        self.image.set_colorkey(BLACK)
+        num = random.randrange(0,4)
+        self.image_org = meteor_img[num]#pygame.transform.scale(meteor, (50,42))
+        self.image_org.set_colorkey(BLACK)
+        self.image = self.image_org
         # self.image.fill(RED)
-        self.rect = self.image.get_rect()
-        self.radius = 20
+        self.rect = self.image_org.get_rect()
+        self.radius = radius_lvl[num]
+        # self.radius = 20
         # pygame.draw.circle(self.image, BLUE, self.rect.center, self.radius)
-        self.rect.x = random.randrange(0,WIDTH-20)
+        self.rect.x = random.randrange(0,WIDTH-40)
         self.rect.y = random.randrange(-100,-40)
         self.speed = random.randrange(4,12)
+        self.rot = 0
+        self.rot_speed = random.randrange(-8,8)
+        self.last_update = pygame.time.get_ticks()
+    def rotate(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > 50:
+            self.last_update = now
+            self.rot = (self.rot + self.rot_speed) % 360
+            new_image = pygame.transform.rotate(self.image_org, self.rot)
+            old_center = self.rect.center
+            self.image = new_image
+            self.rect = self.image.get_rect()
+            self.rect.center = old_center
     def update(self):
+        self.rotate()
         self.rect.y += self.speed
         if self.rect.y > HEIGHT+10:
             self.rect.x = random.randrange(0,WIDTH-20)
@@ -78,7 +103,7 @@ class Bullets(pygame.sprite.Sprite):
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((HEIGHT,WIDTH))
-pygame.display.set_caption("----")
+pygame.display.set_caption("SHMUP!")
 clock = pygame.time.Clock()
 
 # Locations
@@ -86,7 +111,11 @@ img = os.path.dirname("/home/rhithick/Desktop/NITT/Code/Python3/Game_dev/images/
 background = pygame.image.load(os.path.join(img, "starfield.jpg")).convert()
 background_rect = background.get_rect()
 ship = pygame.image.load(os.path.join(img, "playerShip2_green.png")).convert()
-meteor = pygame.image.load(os.path.join(img, "meteorBrown_big1.png")).convert()
+meteor_img = []
+meteor_list = ['meteorBrown_big1.png', 'meteorBrown_med1.png', 'meteorBrown_small1.png', 'meteorBrown_tiny1.png']
+for imge in meteor_list:
+    meteor_img.append(pygame.image.load(os.path.join(img, imge)).convert())
+radius_lvl = [20, 16, 10, 5]
 laser = pygame.image.load(os.path.join(img, "laserRed01.png")).convert()
 
 # Sprites
@@ -100,6 +129,7 @@ for i in range(0,8):
     all_sprites.add(enemy)
     all_oponnents.add(enemy)
 
+score = 0
 # Game loop
 running = True
 while running:
@@ -112,8 +142,9 @@ while running:
             if event.key == pygame.K_SPACE:
                 player.shoot()
     all_sprites.update()
-    hits = pygame.sprite.groupcollide(all_bullets, all_oponnents, True, True)
+    hits = pygame.sprite.groupcollide(all_oponnents, all_bullets, True, True)
     for hit in hits:
+        score += 50 - hit.radius
         enemy = Enemy()
         all_sprites.add(enemy)
         all_oponnents.add(enemy)
@@ -123,5 +154,7 @@ while running:
     screen.fill(BLACK)
     screen.blit(background, background_rect)
     all_sprites.draw(screen)
+    buf = "Score: " + str(score)
+    draw_text(buf, screen, 18, WIDTH/2, 10)
     pygame.display.flip()
 pygame.quit()
